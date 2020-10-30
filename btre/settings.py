1d02bdbd5135750c1406fe1efe2bed2f811a242c
rdaly3 <rscottdaly@gmail.com>
2020-10-30 10:23:22 -0600

    Finished app commit2

--- btre/settings.py
+++ btre/settings.py
@@ -151,5 +151,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'desertcitysoundtrack@gmail.com'
-EMAIL_HOST_PASSWORD = 'alexiloveyou'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
